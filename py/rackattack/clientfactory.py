86a2e55954ff4b8f5e005296e2ae336b6be627a0
Eliran Levi <eliran@stratoscale.com>
2016-01-26 16:43:10 +0200

    Allow passing the rackattack connection string as an argument to the client factory

--- py/rackattack/clientfactory.py
+++ py/rackattack/clientfactory.py
@@ -5,11 +5,13 @@ from rackattack.tcp import client
 _VAR_NAME = "RACKATTACK_PROVIDER"
 
 
-def factory():
-    if _VAR_NAME not in os.environ:
-        raise Exception(
-            "The environment variable '%s' must be defined properly" % _VAR_NAME)
-    request, subscribe, http = os.environ[_VAR_NAME].split("@@")
+def factory(connectionString=None):
+    if connectionString is None:
+        if _VAR_NAME not in os.environ:
+            raise Exception(
+                "The environment variable '%s' must be defined properly" % _VAR_NAME)
+        connectionString = os.environ[_VAR_NAME]
+    request, subscribe, http = connectionString.split("@@")
     return client.Client(
         providerRequestLocation=request,
         providerSubscribeLocation=subscribe,
